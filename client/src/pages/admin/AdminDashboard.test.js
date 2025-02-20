60ae5503bb4d339bc552b26408f8d8282c654bb9
Rayson <122922486+raysonchia@users.noreply.github.com>
2025-02-21 03:17:03 +0800

    Add AdminDashboard tests

---
+++ client/src/pages/admin/AdminDashboard.test.js
@@ -0,0 +1,65 @@
+import React from 'react';
+import { render, screen } from '@testing-library/react';
+import AdminDashboard from './AdminDashboard';
+import { MemoryRouter } from 'react-router-dom';
+import { useAuth } from '../../context/auth';
+import "@testing-library/jest-dom";
+
+jest.mock('../../context/auth', () => ({
+    useAuth: jest.fn(),
+}));
+  
+jest.mock('../../components/Layout', () => ({ children }) => (
+    <div data-testid="layout-mock">{ children }</div>
+));
+  
+jest.mock('../../components/AdminMenu', () => () => (
+    <div data-testid="admin-menu-mock" />
+));
+
+describe('AdminDashboard', () => {
+    const mockAuth = {
+        user: {
+          name: 'John Doe',
+          email: 'john@gmail.com',
+          phone: '84327890',
+        },
+    };
+
+    beforeEach(() => {
+        useAuth.mockImplementation(() => [mockAuth]);
+        render(
+            <MemoryRouter>
+              <AdminDashboard />
+            </MemoryRouter>
+        );
+    });
+    
+    afterEach(() => {
+        jest.clearAllMocks();
+    });
+
+    test('renders the Layout component', () => {
+        expect(screen.getByTestId('layout-mock')).toBeInTheDocument();
+    });
+
+    test('renders the AdminMenu component', () => {
+        expect(screen.getByTestId('admin-menu-mock')).toBeInTheDocument();
+    });
+
+    test('displays correct user information from auth context', () => {
+        expect(screen.getByRole('heading', { name: /admin name : john doe/i }))
+          .toBeInTheDocument();
+        expect(screen.getByRole('heading', { name: /admin email : john@gmail\.com/i }))
+          .toBeInTheDocument();
+        expect(screen.getByRole('heading', { name: /admin contact : 84327890/i }))
+          .toBeInTheDocument();
+    });
+
+    test('does not display sensitive user data', () => {
+        expect(screen.queryByText(/password/i)).toBeNull();
+        expect(screen.queryByText(/address/i)).toBeNull();
+        expect(screen.queryByText(/answer/i)).toBeNull();
+    });
+
+});
