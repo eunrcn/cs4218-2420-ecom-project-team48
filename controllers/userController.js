1758b95ee4d28a91ee798b9481790f445420c0eb
Rayson <122922486+raysonchia@users.noreply.github.com>
2025-03-09 19:42:52 +0800

    Add unit tests for userController

--- controllers/userController.js
+++ controllers/userController.js
@@ -2,19 +2,19 @@ import UserModel from "../models/userModel.js";
 
 // get all users
 export const getUsersController = async (req, res) => {
-    try {
-      const users = await UserModel.find({});
-      res.status(200).send({
-        success: true,
-        message: "Users retrieved successfully",
-        users,
-      });
-    } catch (error) {
-      console.error(error);
-      res.status(500).send({
-        success: false,
-        message: "Error retrieving users",
-        error: error.message,
-      });
-    }
-  };
+  try {
+    const users = await UserModel.find({}).select("_id name email role");
+    res.status(200).send({
+      success: true,
+      message: "Users retrieved successfully",
+      users,
+    });
+  } catch (error) {
+    console.error(error);
+    res.status(500).send({
+      success: false,
+      message: "Error retrieving users",
+      error: error.message,
+    });
+  }
+};
