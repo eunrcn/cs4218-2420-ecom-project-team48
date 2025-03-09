31f06dc754e2d726995aaa049550be7ac5cd69a3
Rayson <122922486+raysonchia@users.noreply.github.com>
2025-03-09 20:32:16 +0800

    Add AdminMenu unit test

--- client/src/components/AdminMenu.test.js
+++ client/src/components/AdminMenu.test.js
@@ -1,48 +1,52 @@
-import React from 'react';
-import { render, screen } from '@testing-library/react';
-import { MemoryRouter } from 'react-router-dom';
-import '@testing-library/jest-dom';
+import React from "react";
+import { render, screen } from "@testing-library/react";
+import { MemoryRouter } from "react-router-dom";
+import "@testing-library/jest-dom";
 
-import AdminMenu from './AdminMenu';
+import AdminMenu from "./AdminMenu";
 
 const ADMIN_LINKS = [
-    {
-      path: '/dashboard/admin/create-category',
-      name: 'Create Category',
-    },
-    {
-      path: '/dashboard/admin/create-product',
-      name: 'Create Product',
-    },
-    {
-      path: '/dashboard/admin/products',
-      name: 'Products',
-    },
-    {
-      path: '/dashboard/admin/orders',
-      name: 'Orders',
-    },
+  {
+    path: "/dashboard/admin/create-category",
+    name: "Create Category",
+  },
+  {
+    path: "/dashboard/admin/create-product",
+    name: "Create Product",
+  },
+  {
+    path: "/dashboard/admin/products",
+    name: "Products",
+  },
+  {
+    path: "/dashboard/admin/orders",
+    name: "Orders",
+  },
+  {
+    path: "/dashboard/admin/users",
+    name: "Users",
+  },
 ];
 
-describe('AdminMenu Component', () => {
-    beforeEach(() => {
-        render(
-          <MemoryRouter>
-            <AdminMenu />
-          </MemoryRouter>
-        );
-    });
-
-    test('renders the "Admin Panel" heading', () => {
-        const heading = screen.getByText(/admin panel/i);
-        expect(heading).toBeInTheDocument();
-    });
-
-    test.each(ADMIN_LINKS)(
-        'renders $name link with correct path',
-        ({ path, name }) => {
-          const link = screen.getByRole('link', { name: new RegExp(name, 'i') });
-          expect(link).toHaveAttribute('href', path);
-        }
+describe("AdminMenu Component", () => {
+  beforeEach(() => {
+    render(
+      <MemoryRouter>
+        <AdminMenu />
+      </MemoryRouter>
     );
+  });
+
+  test('renders the "Admin Panel" heading', () => {
+    const heading = screen.getByText(/admin panel/i);
+    expect(heading).toBeInTheDocument();
+  });
+
+  test.each(ADMIN_LINKS)(
+    "renders $name link with correct path",
+    ({ path, name }) => {
+      const link = screen.getByRole("link", { name: new RegExp(name, "i") });
+      expect(link).toHaveAttribute("href", path);
+    }
+  );
 });
