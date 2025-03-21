65291aa3e3b93698dee280579713857fc84ec8af
eunrcn <eunicekoh46@gmail.com>
2025-03-21 13:39:53 +0800

    Add UI tests for Orders

---
+++ tests/ui/orders.spec.js
@@ -0,0 +1,15 @@
+import { test, expect } from "@playwright/test";
+
+test("test", async ({ page }) => {
+  await page.goto("http://localhost:3000/");
+  await page.getByRole("link", { name: "Login" }).click();
+  await page.getByRole("textbox", { name: "Enter Your Email" }).click();
+  await page
+    .getByRole("textbox", { name: "Enter Your Email" })
+    .fill("cs4218@test.com");
+  await page.getByRole("textbox", { name: "Enter Your Password" }).click();
+  await page
+    .getByRole("textbox", { name: "Enter Your Password" })
+    .fill("cs4218@test.com");
+  await page.getByRole("button", { name: "LOGIN" }).click();
+});
