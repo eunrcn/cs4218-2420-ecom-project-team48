50803e59cb7f4f106626835ec973fe271ada894e
eunrcn <eunicekoh46@gmail.com>
2025-03-22 17:52:39 +0800

    Add UI tests for AdminOrders: Admin View Orders

--- tests/ui/orders.spec.js
+++ tests/ui/orders.spec.js
@@ -85,3 +85,40 @@ test("admin should be able to update order status", async ({ page }) => {
     .click();
 });
 
+test("admin should be able to view orders", async ({ page }) => {
+  await page.getByRole("link", { name: "Login" }).click();
+  await page.getByRole("textbox", { name: "Enter Your Email" }).click();
+  await page
+    .getByRole("textbox", { name: "Enter Your Email" })
+    .fill("admin@admin.com");
+  await page.getByRole("textbox", { name: "Enter Your Password" }).click();
+  await page
+    .getByRole("textbox", { name: "Enter Your Password" })
+    .fill("admin");
+  await page.getByRole("button", { name: "LOGIN" }).click();
+  await page.getByRole("button", { name: "admin" }).click();
+  await page.getByRole("link", { name: "Dashboard" }).click();
+  await page.getByRole("link", { name: "Orders" }).click();
+
+    await expect(page.getByText("All Orders#StatusBuyer")).toBeVisible();
+    await expect(
+      page
+        .locator("div")
+        .filter({
+          hasText: /^NUS T-shirtPlain NUS T-shirt for salePrice : 4\.99$/,
+        })
+        .first()
+    ).toBeVisible();
+    await expect(
+      page
+        .locator("div")
+        .filter({ hasText: /^LaptopA powerful laptopPrice : 1499\.99$/ })
+        .first()
+    ).toBeVisible();
+    await expect(
+      page
+        .locator("div")
+        .filter({ hasText: /^LaptopA powerful laptopPrice : 1499\.99$/ })
+        .nth(2)
+    ).toBeVisible();
+});
