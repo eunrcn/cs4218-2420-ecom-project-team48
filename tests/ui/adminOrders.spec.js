2843f4bd6ee763a37c61f364f63cbaaa22b1eb44
eunrcn <eunicekoh46@gmail.com>
2025-03-23 16:37:07 +0800

    Add UI tests for Profile: Update address and number

--- tests/ui/adminOrders.spec.js
+++ tests/ui/adminOrders.spec.js
@@ -4,7 +4,7 @@ test.beforeEach(async ({ page }) => {
   await page.goto(".");
 });
 
-test("admin should be able to update order status", async ({ page }) => {
+test("admin should be able to update order status to not processed", async ({ page }) => {
   await page.getByRole("link", { name: "Login" }).click();
   await page.getByRole("textbox", { name: "Enter Your Email" }).click();
   await page
@@ -19,12 +19,24 @@ test("admin should be able to update order status", async ({ page }) => {
   await page.getByRole("link", { name: "Dashboard" }).click();
   await page.getByRole("link", { name: "Orders" }).click();
 
-  await page.getByText("Not Process").click();
-  await page
-    .locator("div")
-    .filter({ hasText: "Not ProcessProcessingNot" })
-    .nth(1)
-    .click();
+  const statuses = ["Shipped", "Not Process", "Processing", "Delivered", "Cancelled"];
+
+  for (const status of statuses) {
+    if (await page.getByText(status).isVisible()) {
+      await page
+        .getByTestId("status-67a21938cf4efddf1e5358d1")
+        .getByText(status)
+        .click();
+        await page
+          .getByTestId("status-67a21938cf4efddf1e5358d1")
+          .getByTitle("Not Process")
+          .click();
+        await page
+          .getByRole("cell", { name: "Not Process Not Process" })
+          .click();
+      break;
+    }
+  }
 });
 
 test("admin should be able to view orders", async ({ page }) => {
@@ -65,6 +77,32 @@ test("admin should be able to view orders", async ({ page }) => {
   ).toBeVisible();
 });
 
-
-
-// 2 more 
+test("admin should be able to add and remove items from cart", async ({ page }) => {
+  await page.getByRole("link", { name: "Login" }).click();
+  await page
+    .getByRole("textbox", { name: "Enter Your Email" })
+    .fill("admin@admin.com");
+  await page.getByRole("textbox", { name: "Enter Your Password" }).click();
+  await page
+    .getByRole("textbox", { name: "Enter Your Password" })
+    .fill("admin");
+  await page.getByRole("button", { name: "LOGIN" }).click();
+  await page.locator(".card-name-price > button:nth-child(2)").first().click();
+  await page
+    .locator(
+      "div:nth-child(2) > .card-body > div:nth-child(3) > button:nth-child(2)"
+    )
+    .click();
+  await page.getByRole("link", { name: "Cart" }).click();
+  await page
+    .getByRole("heading", { name: "Hello admin You Have 2 items" })
+    .click();
+  await page.getByRole("button", { name: "Remove" }).nth(1).click();
+  await page
+    .getByRole("heading", { name: "Hello admin You Have 1 items" })
+    .click();
+  await page.getByRole("button", { name: "Remove" }).click();
+  await page
+    .getByRole("heading", { name: "Hello admin Your Cart Is Empty" })
+    .click();
+});
